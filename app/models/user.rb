56674b294008cbb9b0991f2e1b38f8456c12aa93
Martin Ziserman <martin@mbp-de-martin.home>
2018-03-06 14:06:29 +0100

    models for stream/subscribtion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :streams
+
+  has_many :subscriptions
+  has_many :subscribed_to, through: :subscriptions, source: :stream
 end
