f74a57a14785a5410ac4ed0a78316080fcc37bf6
Boris Laporte <boris.laporte@protonmail.com>
2018-03-12 22:55:54 +0100

    init Docker :dog:

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304210232) do
+ActiveRecord::Schema.define(version: 20180312083624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "stream_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stream_id"], name: "index_follows_on_stream_id"
+    t.index ["user_id"], name: "index_follows_on_user_id"
+  end
+
+  create_table "streams", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_streams_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +49,6 @@ ActiveRecord::Schema.define(version: 20180304210232) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "follows", "streams"
+  add_foreign_key "follows", "users"
 end
