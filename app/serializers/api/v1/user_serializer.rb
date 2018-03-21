ee516154be9b81cf9d9fdc15b627826cb92c936c
Thomas Victoria <vtom.pro@gmail.com>
2018-03-21 16:17:15 +0100

    putain de virgule

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -2,12 +2,12 @@ class Api::V1::UserSerializer < ActiveModel::Serializer
 
   attributes :id,
              :email,
-             :stream_id
+             :stream_id,
              :first_name,
              :last_name,
              :pseudo,
              :slug
-             
+
   attribute :auth_token, if: -> { instance_options[:auth_token].present? }
 
   def auth_token
