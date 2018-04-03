fcac75a81f77169f246e331feda608506f3aa3e0
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-03 12:31:24 +0200

    fix follow serializer

--- app/serializers/api/v1/follow_serializer.rb
+++ app/serializers/api/v1/follow_serializer.rb
@@ -2,8 +2,6 @@ class Api::V1::FollowSerializer < ActiveModel::Serializer
 
   attributes :id,
              :follower_id,
-             :streamer_id,
-             :created_at,
-             :updated_at
+             :channel_id
 
 end
