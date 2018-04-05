cb9b1c7bf6422265294609cf9655c7b752e8a874
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-05 11:58:40 +0200

    chat base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402205604) do
+ActiveRecord::Schema.define(version: 20180405092046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20180402205604) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.index ["streamer_id"], name: "index_channels_on_streamer_id"
+  end
+
+  create_table "chat_messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_chat_messages_on_user_id"
   end
 
   create_table "follows", force: :cascade do |t|
