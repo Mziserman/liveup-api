b594d330ba049a95aeee9fe11fb9bdabdc0591c7
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-06 12:16:38 +0200

    fix?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://liveup-api.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ '*', 'http://localhost:3000', 'http://127.0.0.1:3000' ]
+  # config.action_cable.url = 'wss://liveup-api.herokuapp.com/cable'
+  # config.action_cable.allowed_request_origins = [ '*', 'http://localhost:3000', 'http://127.0.0.1:3000' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
