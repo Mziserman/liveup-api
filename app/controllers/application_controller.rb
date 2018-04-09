42675cbc9779e74464dccacdef591cfeb4ee9bec
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-09 09:19:05 +0200

    fix auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
   def authenticate_request!
-    if !user_id_in_auth_token? && !expired(auth_token)
+    if !user_id_in_auth_token? || expired(auth_token)
       return head :unauthorized
     end
 
@@ -23,31 +23,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  protected
-
   private
 
   def user_id_in_auth_token?
-    http_auth_token && auth_token && auth_token[:user_id]
-  end
-
-  def user_id_in_refresh_token?
-    http_refresh_token && refresh_token && refresh_token[:user_id]
+    http_auth_token && auth_token && auth_token[:user_id].present?
   end
 
   def http_auth_token
-    @http_token ||= if request.headers['Authorization'].present?
-      request.headers['Authorization'].split(' ').last
+    if request.headers['Authorization'].present?
+      @http_token ||= request.headers['Authorization'].split(' ').last
     else
-
-    end
-  end
-
-  def http_refresh_token
-    @http_refresh_token ||= if request.headers['Refresh'].present?
-      request.headers['Refresh'].split(' ').last
-    else
-
+      false
     end
   end
 
@@ -55,12 +41,24 @@ class ApplicationController < ActionController::Base
     @auth_token ||= ::JsonWebToken.decode(http_auth_token)
   end
 
+  def user_id_in_refresh_token?
+    http_refresh_token && refresh_token && refresh_token[:user_id]
+  end
+
+  def http_refresh_token
+    if request.headers['Refresh'].present?
+      @http_refresh_token ||= request.headers['Refresh'].split(' ').last
+    else
+      false
+    end
+  end
+
   def refresh_token
     @refresh_token ||= ::JsonWebToken.decode(http_refresh_token)
   end
 
   def expired(payload)
-    payload&[:exp].present? && Time.at(payload&[:exp]) > Time.now
+    payload[:exp].present? && Time.at(payload[:exp]) < Time.now
   end
 
   # def set_raven_context
