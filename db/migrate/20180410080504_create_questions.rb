20bea3227fe8cce3a0f8b23b41896f51b4d52422
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-10 12:44:05 +0200

    move default to create table migration

--- db/migrate/20180410080504_create_questions.rb
+++ db/migrate/20180410080504_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
       t.text :content
-      t.integer :score
+      t.integer :score, default: 0
       t.references :user
       t.references :stream
 
