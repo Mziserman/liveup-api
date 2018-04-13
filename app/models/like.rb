f711d939c8cc39e6063cc17053554ea086f12c1a
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-13 11:43:39 +0200

    like count

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   validates :user, uniqueness: { scope: :stream }
 
-  belongs_to :stream
+  belongs_to :stream, counter_cache: true
   belongs_to :user
 end
--- app/serializers/api/v1/stream_serializer.rb
+++ app/serializers/api/v1/stream_serializer.rb
@@ -3,6 +3,11 @@ class Api::V1::StreamSerializer < ActiveModel::Serializer
   attributes :id,
              :channel_id,
              :session_id,
-             :token
+             :token,
+             :likes
+
+  def likes
+    @object.likes_count
+  end
 
 end
