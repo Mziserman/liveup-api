86ff73ff6e5c1d14890bb8808998740f84657ce5
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-13 10:15:02 +0200

    yoloo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://liveup-api.herokuapp.com/cable'
+  config.action_cable.disable_request_forgery_protection = true
+
   config.action_cable.allowed_request_origins = [ '*' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
