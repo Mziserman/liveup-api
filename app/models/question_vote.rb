c29ad8d28b789055ee730ca2b153c712832c9f24
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-13 09:14:32 +0200

    broadcast question vote on question channel

--- app/models/question_vote.rb
+++ app/models/question_vote.rb
@@ -1,4 +1,17 @@
 class QuestionVote < ApplicationRecord
+
+  after_commit do
+    ActionCable
+      .server
+      .broadcast("stream_#{question.stream_id}_question_channel",
+                 id: question.id,
+                 user_id: question.user_id,
+                 created_at: questioncreated_at,
+                 score: question.question_votes_count,
+                 content: questioncontent,
+                 pseudo: question.user.pseudo)
+  end
+
   validates :user, uniqueness: { scope: :question }
 
   belongs_to :user
