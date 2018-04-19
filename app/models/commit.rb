233e4ad255ece0b5db5f609c8b4e66276a5265fe
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-19 11:50:07 +0200

    fix files

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,7 +2,7 @@ class Commit < ApplicationRecord
 
   belongs_to :shared_file
 
-  before_create create_version
+  before_create :create_version
 
   def create_version
     version = file.commit.last.version + 1
