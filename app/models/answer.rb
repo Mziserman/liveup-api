166d2976988420e23b6544e3491ca4ae2836c37d
Martin Ziserman <martin@mbp-de-martin.home>
2018-04-23 11:20:20 +0200

    always broadcast question and answers in question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,6 @@
 class Answer < ApplicationRecord
   after_commit do
-    ActionCable
-      .server
-      .broadcast("stream_#{stream_id}_question_channel",
-                 id: id,
-                 user_id: user_id,
-                 created_at: created_at,
-                 score: answer_votes_count,
-                 content: content,
-                 pseudo: user.pseudo)
+    question.broadcast
   end
 
   default_scope { order(answer_votes_count: :desc) }
--- app/models/answer_vote.rb
+++ app/models/answer_vote.rb
@@ -1,15 +1,7 @@
 class AnswerVote < ApplicationRecord
 
   after_commit do
-    ActionCable
-      .server
-      .broadcast("stream_#{answer.stream_id}_question_channel",
-                 id: answer.id,
-                 user_id: answer.user_id,
-                 created_at: answer.created_at,
-                 score: answer.answer_votes_count,
-                 content: answer.content,
-                 pseudo: answer.user.pseudo)
+    answer.question.broadcast
   end
 
   validates :user, uniqueness: { scope: :answer }
