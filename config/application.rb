9cd83475e31b69bebce4d8bc73991e3303ae1c0a
Boris Laporte <boris.laporte@protonmail.com>
2018-05-21 16:25:36 +0200

    :tada: Aws elastic beanstalk ready :tada:

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,6 @@ module Liveup
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
-    config.action_cable.disable_request_forgery_protection = true
-    config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://liveup-api.herokuapp.com/cable'
-  # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # config.action_cable.allowed_request_origins = [ '*' ]
 
