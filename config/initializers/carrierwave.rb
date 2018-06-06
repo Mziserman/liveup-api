f7dcf8539b96e7e724047a513b791468b0dba920
Thomas Victoria <thomas@askmona.fr>
2018-06-06 16:30:33 +0200

    fix(carrierwave): S3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],     # required
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # required
-    region:                ENV["AWS_REGION"]             # optional, defaults to 'us-east-1'
+    region:                'eu-west-3'             # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'liveup-avatar'               # required
   config.storage = :fog
