515b03c3d2855a55222fc86b4d8bb93d7b76b800
Martin Ziserman <martin@mbp-de-martin.home>
2018-06-07 14:08:25 +0200

    use streamer channel for un/follow and un/like

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,13 +2,25 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User", foreign_key: :follower_id
   belongs_to :channel
 
-  after_commit :broadcast
-  def broadcast
+  after_create_commit :broadcast_create
+  def broadcast_create
+    stream = channel.streams.first
     ActionCable
       .server
-      .broadcast("stream_#{stream_id}_streamer_channel",
+      .broadcast("stream_#{stream.id}_streamer_channel",
+        event: "follow",
         pseudo: follower.pseudo,
         color: follower.color)
   end
 
+  after_destroy_commit :broadcast_destroy
+  def broadcast_destroy
+    stream = channel.streams.first
+    ActionCable
+      .server
+      .broadcast("stream_#{stream.id}_streamer_channel",
+        event: "unfollow",
+        pseudo: follower.pseudo,
+        color: follower.color)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,24 @@ class Like < ApplicationRecord
 
   belongs_to :stream, counter_cache: true
   belongs_to :user
+
+  after_create_commit :broadcast_create
+  def broadcast_create
+    ActionCable
+      .server
+      .broadcast("stream_#{stream_id}_streamer_channel",
+        event: "like",
+        pseudo: user.pseudo,
+        color: user.color)
+  end
+
+  after_destroy_commit :broadcast_destroy
+  def broadcast_destroy
+    ActionCable
+      .server
+      .broadcast("stream_#{stream_id}_streamer_channel",
+        event: "unlike",
+        pseudo: user.pseudo,
+        color: user.color)
+  end
 end
